445fc2beb6d6c9a888af582b8ec5309c72475fc8
elect-gombe <kenicecandy0141@yahoo.co.jp>
2016-12-04 11:21:20 +0900

    first commit
    add some source files

---
+++ audio.c
@@ -0,0 +1,125 @@
+#include "audio.h"
+#include <stdio.h>
+
+
+/*
+ *          *                                
+ *         * ** 
+ *        *    **                                 
+ *       *       ******                                  
+ *      *              ******                           
+ *     *                     *                      
+ *    *                       *
+ *      (a)  (d)      (s)    (r)                       
+ */
+
+#define SQ_TABLE_N2 5
+
+const int8_t _sq_table[1<<SQ_TABLE_N2]={
+    100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,-100,
+    };
+
+#define FREQMUL_N2 8
+const unsigned int key_to_freq_table[]={
+    33,35,38,40,42,45,47,50,53,56,60,63,67,71,75,80,84,89,95,100,106,113,119,126,134,142,150,159,169,179,189,201,213,225,239,253,268,284,301,319,338,358,379,401,425,451,477,506,536,568,601,637,675,715,758,803,851,901,955,1011,1072,1135,1203,1274,1350,1430,1515,1606,1701,1802,1909,2023,2143,2271,2406,2549,2700,2861,3031,3211,3402,3604,3819,4046,4286,4541,4811,5098,5401,5722,6062,6422,6804,7209,7638,8092,8573,9083,9623,10195,10801,11444,12124,12845,13609,14418,15275,16184,17146,18165,19246,20390,21602,22887,24248,25690,27217,28836,30551,32367,34292,36331,38491,40780,43205,45774,48496,51380
+};
+
+#define ENVE_EXP_TABLE_N2 7
+#define ENVE_EXP_TABLE_MAX_N2 8
+const unsigned char enve_exp_table[]={
+    250,239,229,219,210,201,193,184,177,169,162,155,148,142,136,130,125,119,114,109,105,100,96,92,88,84,81,77,74,71,68,65,62,60,57,55,52,50,48,46,44,42,40,39,37,35,34,32,31,30,28,27,26,25,24,23,22,21,20,19,18,18,17,16,15,15,14,14,13,12,12,11,11,10,10,10,9,9,8,8,8,7,7,7,6,6,6,6,5,5,5,5,5,4,4,4,4,4,4,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1
+};
+
+const wavetable_t sq_wavetable={
+    .table = _sq_table,
+    .sizeofbuf_n2 = SQ_TABLE_N2,
+};
+
+
+const enve_t enve1={
+    .a_vel = 10000,
+    .d_vel = 100,
+    .s_vel = 50,
+    .r_vel = 8000,
+    .a_time = 6,
+    .ad_time = 25,
+};
+
+sound_t sound[SOUND_CH];
+
+void calcsound(sound_t *s,unsigned char *buff);
+
+void mktone(unsigned int keyofTone,unsigned int len,unsigned int vel,size_t idx){
+    sound[idx].active = 1;
+    sound[idx].len = len;
+    sound[idx].theta = 0;
+    sound[idx].wt = &sq_wavetable;
+    sound[idx].tone_freq = key_to_freq_table[keyofTone]>>(8-sound[idx].wt->sizeofbuf_n2);
+    sound[idx].time = 0;
+    sound[idx].enve = &enve1;
+    sound[idx].enve_val = 0;
+    sound[idx].vel = vel;
+}
+
+void soundTask(unsigned char *buff){
+    size_t i;
+    for(i=0;i<SOUND_CH;i++){
+        if(sound[i].active == 1)
+      calcsound(&sound[i],buff);
+    }
+}
+
+void calcsound(sound_t *s,unsigned char *buff){
+    unsigned int idx;
+    unsigned int i;
+    unsigned int enve;
+    unsigned int time_div_n;
+    
+    for(i=0;i<SIZEOFSOUNDBF >> 1;){
+        time_div_n = (s->time&(0xFF>>(8-ENV_INTR_N2-1)));
+        if(time_div_n==0){
+            calcEnve(s);
+        }
+        s->time++;
+        s->theta += s->tone_freq;
+        s->theta &= (0xFFFF >> (16-OVERSAMPLING_N2-s->wt->sizeofbuf_n2));
+
+        idx = (s->theta >> (OVERSAMPLING_N2));
+        enve = (s->enve_val *(time_div_n) + (16- time_div_n)*s->prev_enve_val);
+        enve = enve_exp_table[(1<<ENVE_EXP_TABLE_N2)-((enve * s->vel) >> (ENV_INTR_N2 + SOUND_VEL_MAX_N2+ENVE_MAX_N2-ENVE_EXP_TABLE_N2))];
+        buff[i] += (enve * s->wt->table[idx]) >> ENVE_EXP_TABLE_MAX_N2;
+        i++;
+
+        s->time++;
+        s->theta += s->tone_freq;
+        s->theta &= (0xFFFF >> (16-OVERSAMPLING_N2-s->wt->sizeofbuf_n2));
+
+        idx = (s->theta >> (OVERSAMPLING_N2));
+        buff[i] += (enve * s->wt->table[idx]) >> ENVE_EXP_TABLE_MAX_N2;
+        i++;
+    }
+}
+
+int calcEnve(sound_t *s){
+    const enve_t *e = s->enve;
+    unsigned int time = s->time>>ENV_INTR_N2;
+
+    s->prev_enve_val = s->enve_val;
+
+    if(time < e->a_time){
+        s->enve_val += e->a_vel;
+    }else if(time < e->ad_time){
+        s->enve_val -= e->d_vel;
+    }else if(time > s->len){
+        s->enve_val -= e->r_vel;
+    }else{
+        s->enve_val -= e->s_vel;
+    }
+    
+    if(s->enve_val > (0x1<<31)){
+        s->enve_val = 0;
+    }
+    
+    return s->enve_val;
+}
+
---
+++ newmain.c
@@ -0,0 +1,108 @@
+
+#define _SUPPRESS_PLIB_WARNING
+#include "audio.h"
+#include <plib.h>
+#include "composite32-high4.h"
+#include "lib_composite32-high.h"
+#include <stdint.h>
+
+//外付けクリスタル with PLL (15倍)
+#pragma config PMDL1WAY = OFF, IOL1WAY = OFF
+#pragma config FPLLIDIV = DIV_1, FPLLMUL = MUL_15, FPLLODIV = DIV_1
+#pragma config FNOSC = PRIPLL, FSOSCEN = OFF, POSCMOD = XT, OSCIOFNC = OFF
+#pragma config FPBDIV = DIV_1, FWDTEN = OFF, JTAGEN = OFF, ICESEL = ICS_PGx1
+
+#define SAMPLING_FREQ 16000
+#define OUTPUT_FREQ 100000
+#define CLOCK_FREQ 53800000
+
+
+typedef unsigned int uint;
+
+void audiotask(void);
+
+//sounddata配列　低いド?3オクターブ分の周期カウンタ値、PR3に書き込むと音程設定される
+unsigned char sounddata[SIZEOFSOUNDBF]={0};
+
+void main(void){
+  int i;
+  uint prevtrans;
+
+  //ポートの初期設定
+  TRISA = 0x0010; // RA4は入力
+  CNPUA = 0x0010; // RA4をプルアップ
+  ANSELA = 0x0000; // 全てデジタル
+  TRISB = KEYSTART | KEYFIRE | KEYUP | KEYDOWN | KEYLEFT | KEYRIGHT;// ボタン接続ポート入力設定
+  CNPUBSET=KEYSTART | KEYFIRE | KEYUP | KEYDOWN | KEYLEFT | KEYRIGHT;// プルアップ設定
+  ANSELB = 0x0000; // 全てデジタル
+  LATACLR=2;// RA1=0（ボタンモード）
+
+  RPB13R=5;//RPB13ピンにOC4を割り当て
+  OC4R=0;
+  OC4CON=0x000e;// Timer3ベース、PWMモード
+  OC4CONSET=0x8000;//OC4スタート
+  T3CON=0x0000;// プリスケーラ1:1
+  PR3=256;
+  T3CONSET=0x8000;// タイマ3スタート
+    
+  T4CONbits.SIDL = 0;
+  T4CONbits.TCKPS = 3;
+  T4CONbits.T32 = 0;
+  T4CONbits.TCS = 0;
+  TMR4 = 0;
+  PR4 = CLOCK_FREQ / 8 / SAMPLING_FREQ;
+  T4CONbits.ON = 1;
+    
+  DmaChnOpen(0, 0, DMA_OPEN_AUTO);
+
+  DmaChnSetEventControl(0, DMA_EV_START_IRQ(_TIMER_4_IRQ));
+ 
+  DmaChnSetTxfer(0, sounddata, (void*)&OC4RS, sizeof(sounddata), 1, 1);
+ 
+  DmaChnEnable(0);
+    
+  init_composite(); // ビデオ出力システムの初期化
+
+  while(1){
+      for(i=0;i<SOUND_CH;i++)
+    mktone(61 + i*6,1020,120+i*10,i);   
+  
+    for(i=0;i<=30;i++){
+      //60分のn秒ウェイト
+      audiotask();      
+      drawcount=0;
+      
+      while(!drawcount);
+    }
+  }
+}
+
+
+void audiotask(void){
+  static uint prevtrans=1;
+  uint8_t *buff;
+
+  buff = NULL;
+
+  if(DmaChnGetEvFlags(0)&DMA_EV_SRC_HALF){
+    DmaChnClrEvFlags(0,DMA_EV_SRC_HALF);
+    if(prevtrans==2){
+      prevtrans=1;
+      buff = &sounddata[0];
+    }
+  }else if(DmaChnGetEvFlags(0)&DMA_EV_SRC_FULL){
+    DmaChnClrEvFlags(0,DMA_EV_SRC_FULL);
+    if(prevtrans==1){
+      prevtrans=2;
+      buff = &sounddata[SIZEOFSOUNDBF/2];
+    }
+  }
+
+  if(buff){
+      int i;
+      for(i=0;i<SIZEOFSOUNDBF/2;i++){
+          buff[i]=128;
+      }
+      soundTask(buff);
+  }
+}
